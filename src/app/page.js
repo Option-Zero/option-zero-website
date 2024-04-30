5f59c8a03eb36d89e2c264247c7ba6700625c86d
Jaime Curtis <jaime@optionzero.co>
2024-04-30 14:18:52 -0700

    Add example of styling with emotion

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+'use client';
+import styled from '@emotion/styled'
+
+const StyledMain = styled.button`
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
+
 export default function Home() {
   return (
-    <main>
+    <StyledMain>
       <div>
         Hello world
       </div>
-    </main>
+    </StyledMain>
   );
 }
