39074b5fb42eb2f030d928eb0389c682463ea145
maddie <maddiefletcher6@gmail.com>
2024-05-11 14:58:32 -0700

    Add icons, breakpoints to services cards
    remove nextjs template images

--- src/app/_components/ServicesSection.js
+++ src/app/_components/ServicesSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import styled from '@emotion/styled';
 
 const ServiceCardDescription = styled.p`
@@ -12,7 +13,7 @@ const ServiceCardDescription = styled.p`
 const ServiceCard = ({ className, title, description, icon }) => {
     return (
         <div className={className}>
-            <div>{icon}</div>
+            <img src={icon} alt={icon} />
             <h4>{title}</h4>
             <ServiceCardDescription>{description}</ServiceCardDescription>
         </div>
@@ -24,6 +25,10 @@ const StyledServiceCard = styled(ServiceCard)`
     height: 325px;
     min-height: 0;
     min-width: 0;
+    @media (max-width: 750px) {
+        width: auto;
+        height: auto;
+    }
     border: 1px solid rgba(100, 111, 121, 0.12);
     border-radius: 12px;
     margin: 10px;
@@ -34,6 +39,7 @@ const ServiceCardContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+    margin-top: 20px;
     margin-bottom: 40px;
 `;
 
@@ -54,25 +60,25 @@ const StyledServicesSection = styled.div`
 const SERVICES = [
     {
         title: 'Software Strategy',
-        icon: 'icon four circles',
+        icon: 'icons/FourCircles_Icon.svg',
         description:
             "You have a vision for your business. Now you need the software to bring it to life. We'll help you define your software needs and create a roadmap for long-term impact.",
     },
     {
         title: 'Full-stack Implementation',
-        icon: 'icon folder check',
+        icon: 'icons/FolderCheck_Icon.svg',
         description:
             "Focus on what your business does best; let us handle the programming. We'll build your first prototype, tackle that side project, and create a scalable codebase for future development.",
     },
     {
         title: 'Data Foundations',
-        icon: 'icon data',
+        icon: 'icons/Data_Icon.svg',
         description:
             "Your data is a competitive advantage, but only if you know how to put it to work. We'll architect data pipelines to collect and process your data and use it to accelerate your growth.",
     },
     {
         title: 'Team Building',
-        icon: 'icon triangle',
+        icon: 'icons/Triangle_Icon.svg',
         description:
             "Turn your in-house software team into an engine for growth, or keep it lean with on-demand resources. We'll help you build a recruitment pipeline, develop a strategy-driven culture, and position your team for success.",
     },
